6784e5cb39f45f7fb1f883f45e8fa26e389d15e2
Davide Depau <davide@depau.eu>
2019-11-03 00:24:05 +0100

    pyxis: Update device props from stock ROM

--- device-props.mk
+++ device-props.mk
@@ -1,13 +1,16 @@
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
-    persist.camera.sat.fallback.dist=28 \
-    persist.camera.sat.fallback.dist.d=5 \
-    persist.camera.sat.fallback.luxindex=230 \
-    persist.camera.sat.fallback.lux.d=50
+    persist.camera.cfa.thres.front=0 \
+		persist.camera.cfa.thres.rear=200 \
+		persist.vendor.camera.multicam.hwsync=TRUE \
+		persist.vendor.camera.multicam.fpsmatch=TRUE \
+		persist.vendor.camera.enableAdvanceFeatures=0x3E7 \
+		persist.vendor.camera.multicam.framesync=1 \
+		persist.vendor.camera.multicam=TRUE
 
 # Display density
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.sf.lcd_density=480
+    ro.sf.lcd_density=440
 
 # Display features
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -23,7 +26,3 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # NFC
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.nfc.port=I2C
-
-# Radio
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.vendor.radio.uicc_se_enabled=true
