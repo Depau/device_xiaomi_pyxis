50f3cb5581b0a6b6cbec1f64d17cb73f8bb3d282
Davide Depau <davide@depau.eu>
2019-12-02 20:16:38 +0100

    pyxis: Fix AOSP leftovers

--- lineage_pyxis.mk
+++ lineage_pyxis.mk
@@ -20,10 +20,10 @@ $(call inherit-product, device/xiaomi/pyxis/device.mk)
 TARGET_GAPPS_ARCH := arm64
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_INCLUDE_STOCK_ARCORE := true
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := aosp_pyxis
+PRODUCT_NAME := lineage_pyxis
 PRODUCT_DEVICE := pyxis
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi 9 Lite
